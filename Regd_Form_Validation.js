1876ff42f3fe726ecf4b87b6f30db7f8e83d8ade
Subhankari_Padhi <subhankari.padhi@india.semanticbits.com>
2019-11-19 13:54:22 +0530

    "REplaced_addEventListener_with_onkeyup_function_definition"

--- Regd_Form_Validation.js
+++ Regd_Form_Validation.js
@@ -17,23 +17,10 @@ var password_pttern=/^[A-Z]{1}[A-Za-z_0-9]{5,8}$/;
 //var ConfirmPassword_pattern=;
 
 
-FirstName.addEventListener("blur", fname_fun ,true);
-LastName.addEventListener("blur",lname_fun,true);
-DateOfBirth.addEventListener("blur",dob_fun,true);
-EmailId.addEventListener("blur",email_fun,true);
-Password.addEventListener("blur",password_fun,true);
-ConfirmPassword.addEventListener("blur",cpsw_fun,true);
-
 
 
 
 function validation_function() {
-    //FirstName = document.forms["RegForm"]["fname"];
-    //LastName = document.forms["RegForm"]["lname"];
-    // DateOfBirth = document.forms["RegForm"]["dob"];
-    //EmailId = document.forms["RegForm"]["email_id"];
-    //Password =  document.forms["RegForm"]["Password"];
-    // ConfirmPassword = document.forms["RegForm"]["ConfirmPassword"];
 
 //document.cookies="firstName=FirstName.value";
 //localStorage.setItem("key","value");
@@ -45,59 +32,17 @@ function validation_function() {
     sessionStorage.setItem("password", Password.value);
     sessionStorage.setItem("cpsw", ConfirmPassword.value);
 
+    if (FirstName.value == "") { firstname_validation(); return false; }
 
-    //firstName conditions
-    if (FirstName.value == "") {
-        FirstName.focus();
-        //document.getElementById("fname_error").innerHTML = "Please enter your FirstName.";
-        firstname_validation();
-        return false;
-    }
+    if(LastName.value == "") { lastname_validation(); return false; }
 
-//lastName conditions
-    if(LastName.value == "") {
-        LastName.focus();
-        //document.getElementById("lname_error").innerHTML = "Please enter your LastName";
-        lastname_validation();
-        return false;
-    }
+    if (DateOfBirth.value == "") { dob_validation(); return false;  }
 
+    if( EmailId.value==""){  email_validation(); return false; }
 
+    if (Password.value == "" ) { password_validation(); return false; }
 
-//dob condition
-    if (DateOfBirth.value == "") {
-        //document.getElementById("dob_error").innerHTML = "Please enter your Date of birth.";
-        DateOfBirth.focus();
-        dob_validation();
-        return false;
-    }
-
-//email condition
-    if( EmailId.value==""){
-        //document.getElementById("email_error").innerHTML="Please enter your emailId";
-        EmailId.focus();
-        email_validation();
-        return false;
-    }
-
-
-
-//password condition
-    if (Password.value == "" ) {
-        //document.getElementById("password_error").innerHTML = "this field is manadatory";
-        Password.focus();
-        password_validation();
-        return false;
-    }
-
-//confirmpassword condition
-    if(ConfirmPassword.value == "" ){
-        //document.getElementById("cpsw_error").innerHTML = "Please fill this box";
-        ConfirmPassword.focus();
-        cpsw_validation();
-        return false;
-    }
-
+    if(ConfirmPassword.value == "" ){ cpsw_validation(); return false; }
 
     return true;
 }
@@ -221,84 +166,6 @@ function cpsw_validation(){
 
 
 
-
-
-
-
-//functions for session fields  to blur error messages from page after filling textbox
-function  fname_fun() {
-    if (FirstName.value == "") {
-        FirstName.focus();
-        document.getElementById("fname_error").innerHTML = "Please enter your FirstName.";
-        return false;
-    }
-    else {
-
-        document.getElementById("fname_error").innerHTML = "";
-        return true;
-    }
-}
-function lname_fun() {
-    if(LastName.value =="") {
-        document.getElementById("lname_error").innerHTML = "Please enter your LastName";
-        LastName.focus();
-        return false;
-    }
-    else {
-        document.getElementById("lname_error").innerHTML="";
-        return true;
-    }
-}
-function dob_fun() {
-
-    if (DateOfBirth.value == "") {
-        document.getElementById("dob_error").innerHTML = "Please enter your Date of birth.";
-        DateOfBirth.focus();
-        return false;
-    }
-    else{
-        document.getElementById("dob_error").innerHTML="";
-        return true;
-    }
-}
-function  email_fun()
-
-{
-    if( EmailId.value==""){
-        document.getElementById("email_error").innerHTML="this field cant be empty";
-        EmailId.focus();
-        return false;
-    }
-    else {
-        document.getElementById("email_error").innerHTML = "";
-        return true;
-    }
-}
-function password_fun()
-{
-    if (Password.value == "" ) {
-        document.getElementById("password_error").innerHTML = "this field is manadatory";
-        Password.focus();
-        return false;
-    }
-    else {
-        document.getElementById("password_error").innerHTML = "";
-        return true;
-    }
-}
-
-function cpsw_fun(){
-
-    if (ConfirmPassword.value == ""){
-        document.getElementById("cpsw_error").innerHTML = "This field cant be empty";
-        ConfirmPassword.focus();
-        return false;
-    }
-    else{
-        document.getElementById('cpsw_error').innerHTML="";
-        return true;
-    }
-}
 
 
 
